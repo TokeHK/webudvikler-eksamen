b96a9ee508c839fec4abc0b7f6f00643ecc10779
TokeHK <toha7@videndjurs.net>
2025-10-31 12:34:37 +0100

    a

--- src/admin/Pages/AdminArticles.tsx
+++ src/admin/Pages/AdminArticles.tsx
@@ -128,6 +128,8 @@ const AdminArticles: React.FC = () => {
         <h2 className="mb-6 text-3xl font-bold text-center">Article Admin Panel</h2>
         <p>Der skal være mindst 5 landingpage articles / 5 bliver vist</p>
 
+
+        {/* TITLE */}
         <input
           type="text"
           placeholder="Title"
@@ -136,6 +138,7 @@ const AdminArticles: React.FC = () => {
           className="w-full px-3 py-2 mb-2 border"
         />
 
+        {/* SELECT KATEGORI */}
         <select
           value={newArticle.section}
           onChange={(e) => {
@@ -156,7 +159,9 @@ const AdminArticles: React.FC = () => {
             </option>
           ))}
         </select>
+        
 
+        {/* ARTIKEL AUTHOR */}
         <input
           type="text"
           placeholder="Author"
@@ -165,6 +170,8 @@ const AdminArticles: React.FC = () => {
           className="w-full px-3 py-2 mb-2 border"
         />
 
+
+        {/* ARTIKEL TAGS */}
         <input
           type="text"
           placeholder="Tags 'tag1, tag2, tag3' "
@@ -178,6 +185,8 @@ const AdminArticles: React.FC = () => {
           className="w-full px-3 py-2 mb-2 border"
         />
 
+
+        {/* CONTENT ITEMS = content: [], API layer 2 */}
         <h3 className="mt-4 mb-2">Content Items</h3>
         {newArticle.content.map((contentItem, index) => (
           <div key={index} className="p-4 mb-2">
@@ -197,21 +206,22 @@ const AdminArticles: React.FC = () => {
               <option value="main">Main</option>
             </select>
 
+            {/* IF content item er en specific type */}
             {contentItem.type === "image" && (
               <>
-              <input
-                type="text"
-                placeholder="Image URL"
-                value={contentItem.url || ""}
-                onChange={(e) => {
-                  const updated = [...newArticle.content];
-                  updated[index].url = e.target.value;
-                  setNewArticle({ ...newArticle, content: updated });
-                }}
-                className="w-full px-3 py-2 mb-2 border"
-              />
+                <input
+                  type="text"
+                  placeholder="Image url"
+                  value={contentItem.url || ""}
+                  onChange={(e) => {
+                    const updated = [...newArticle.content];
+                    updated[index].url = e.target.value;
+                    setNewArticle({ ...newArticle, content: updated });
+                  }}
+                  className="w-full px-3 py-2 mb-2 border"
+                />
 
-              <input
+                <input
                   type="text"
                   placeholder="alt tekst"
                   value={contentItem.altText || ""}
@@ -222,10 +232,10 @@ const AdminArticles: React.FC = () => {
                   }}
                   className="w-full px-3 py-2 mb-2 border"
                 />
-
               </>
             )}
 
+            {/* Normal tekst */}
             <textarea
               placeholder="Text"
               value={contentItem.text || ""}
@@ -237,6 +247,7 @@ const AdminArticles: React.FC = () => {
               className="w-full px-3 py-2 mb-2 border"
             />
 
+            {/* MAIN type får fat i contentbody: [], API layer 3*/}
             {contentItem.type === "main" &&
               contentItem.contentbody?.map((bodyItem, idx) => (
                 <div key={idx} className="p-2 mb-2 ml-4 border-l">
@@ -276,25 +287,18 @@ const AdminArticles: React.FC = () => {
                 onClick={() => addContentBodyItem(newArticle, index)}
                 className="px-2 py-1 mt-1 border"
               >
-                Tilføj "main" paragraph + headline
+                Tilføj "main" nested paragraph + headline
               </button>
-            )}
+            )}          
           </div>
         ))}
-        <button
-          type="button"
-          onClick={() => addContentItem("new")}
-          className="px-3 py-2 mb-2 border"
-        >
+        {/* CONTENT ITEMS code end */}
+        <button type="button" onClick={() => addContentItem("new")} className="px-3 py-2 mb-2 border">
           Add Content Item
         </button>
 
         <label className="flex items-center gap-2 mt-4">
-          <input
-            type="checkbox"
-            checked={newArticle.isLandingpage}
-            onChange={(e) => setNewArticle({ ...newArticle, isLandingpage: e.target.checked })}
-          />
+          <input type="checkbox" checked={newArticle.isLandingpage} onChange={(e) => setNewArticle({ ...newArticle, isLandingpage: e.target.checked })}/>
           Landing Page
         </label>
 
@@ -302,194 +306,133 @@ const AdminArticles: React.FC = () => {
           Add Article
         </button>
       </div>
+      {/* END POST article */}
 
+      {/* EDIT articles - mapper igennem */}
       <h2 className="mb-4 text-2xl font-semibold">Edit Articles</h2>
       {articles.map((article, index) => (
         <div key={article._id} className="p-2 mb-20 bg-white border">
           Title, Category, Author, Tags
-          <input
-            type="text"
-            value={article.title}
-            onChange={(e) => handleChange(article._id!, "title", e.target.value)}
-            className="w-full px-3 py-2 mb-2 border"
-            placeholder="Title"
+          {/* EDIT TITLE */}
+          <input type="text" value={article.title} onChange={(e) => handleChange(article._id!, "title", e.target.value)}
+            className="w-full px-3 py-2 mb-2 border" placeholder="Title"
           />
-
-          <select
-            value={article.articleCategory || ""}
-            onChange={(e) => {
+          {/* EDIT SELECT CAATEGORY */}
+          <select value={article.articleCategory || ""} onChange={(e) => {
               const selectedCategory = e.target.value;
               const selectedSection = sections?.find((s) => s.name === selectedCategory);
               handleChange(article._id!, "articleCategory", selectedCategory);
               if (selectedSection) handleChange(article._id!, "section", selectedSection._id);
-            }}
-            className="w-full px-3 py-2 mb-2 border"
-          >
-            <option value="">Select Section</option>
+            }} className="w-full px-3 py-2 mb-2 border">
+              <option value="">Select Section</option>
+              {/* CATEGORY options fra article/section[].name */}
             {sections?.map((s) => (
               <option key={s._id} value={s.name}>
                 {s.name}
               </option>
             ))}
           </select>
-
-          <input
-            type="text"
-            value={article.author}
-            onChange={(e) => handleChange(article._id!, "author", e.target.value)}
-            className="w-full px-3 py-2 mb-2 border"
-            placeholder="Author"
-          />
-
-          <input
-            type="text"
-            value={Array.isArray(article.tags) ? article.tags.join(", ") : article.tags}
+          
+          {/* EDIT AUTHOR */}
+          <input type="text" value={article.author} onChange={(e) => handleChange(article._id!, "author", e.target.value)}
+            className="w-full px-3 py-2 mb-2 border" placeholder="Author" />
+          {/* EDIT TAGS */}
+          <input type="text" value={Array.isArray(article.tags) ? article.tags.join(", ") : article.tags} 
             onChange={(e) =>
-              handleChange(
-                article._id!,
-                "tags",
-                e.target.value.split(",").map((t) => t.trim())
-              )
-            }
-            className="w-full px-3 py-2 mb-2 border"
-            placeholder="Tags"
+              handleChange( article._id!, "tags", e.target.value.split(",").map((t) => t.trim()))
+            } className="w-full px-3 py-2 mb-2 border" placeholder="Tags"
           />
 
+          {/* EDIT CONTENT ITEMS */}
           <h3 className="mt-2 mb-2">Content Items</h3>
           {article.content.map((contentItem, index) => (
             <div key={index} className="p-4 mb-2 border rounded-md">
-              <select
-                value={contentItem.type}
-                onChange={(e) => {
-                  const updated = [...article.content];
-                  updated[index].type = e.target.value;
-                  handleChange(article._id!, "content", updated);
-                }}
-                className="w-full px-3 py-2 mb-2 border"
-              >
+              <select value={contentItem.type} onChange={(e) => {
+                const updated = [...article.content];
+                updated[index].type = e.target.value;
+                handleChange(article._id!, "content", updated);
+              }} className="w-full px-3 py-2 mb-2 border">
                 <option value="paragraph">Paragraph</option>
                 <option value="image">Image</option>
                 <option value="link">Link</option>
                 <option value="video">Video</option>
                 <option value="main">Main</option>
               </select>
-
+              
+              {/* EDIT CONTENT TYPE */}
               {contentItem.type === "image" && (
                 <>
                 type, url, alt tekst
-                <input
-                  type="text"
-                  placeholder="Image URL"
-                  value={contentItem.url || ""}
-                  onChange={(e) => {
-                    const updated = [...article.content];
-                    updated[index].url = e.target.value;
-                    handleChange(article._id!, "content", updated);
-                  }}
-                  className="w-full px-3 py-2 mb-2 border"
-                />
-                <input
-                  type="text"
-                  placeholder="alt tekst"
-                  value={contentItem.altText || ""}
-                  onChange={(e) => {
-                    const updated = [...article.content];
-                    updated[index].altText = e.target.value;
-                    handleChange(article._id!, "content", updated);
-                  }}
-                  className="w-full px-3 py-2 mb-2 border"
-                />
+                <input type="text" placeholder="Image URL" value={contentItem.url || ""} onChange={(e) => {
+                  const updated = [...article.content];
+                  updated[index].url = e.target.value;
+                  handleChange(article._id!, "content", updated);
+                }} className="w-full px-3 py-2 mb-2 border" />
+                <input type="text" placeholder="alt tekst" value={contentItem.altText || ""} onChange={(e) => {
+                  const updated = [...article.content];
+                  updated[index].altText = e.target.value;
+                  handleChange(article._id!, "content", updated);
+                }} className="w-full px-3 py-2 mb-2 border" />
                 </>
               )}
 
-              <textarea
-                placeholder="Text"
-                value={contentItem.text || ""}
-                onChange={(e) => {
-                  const updated = [...article.content];
-                  updated[index].text = e.target.value;
-                  handleChange(article._id!, "content", updated);
-                }}
-                className="w-full px-3 py-2 mb-2 border"
-              />
-
+              {/* EDIT TEXT layer 2 */}
+              <textarea placeholder="Text" value={contentItem.text || ""} onChange={(e) => {
+                const updated = [...article.content];
+                updated[index].text = e.target.value;
+                handleChange(article._id!, "content", updated);
+              }} className="w-full px-3 py-2 mb-2 border" />
+              {/* EDIT MAIN, ADD CONTENT ITEMS */}
               {contentItem.type === "main" &&
                 contentItem.contentbody?.map((bodyItem, idx) => (
                   <div key={idx} className="p-2 mb-2 ml-4 border-l">
-                    <input
-                      type="text"
-                      placeholder="Headline"
-                      value={bodyItem.headline || ""}
-                      onChange={(e) => {
+                    <input type="text" placeholder="Headline" value={bodyItem.headline || ""} onChange={(e) => {
                         const updated = [...article.content];
                         updated[index].contentbody![idx] = {
                           ...updated[index].contentbody![idx],
                           headline: e.target.value,
                         };
                         handleChange(article._id!, "content", updated);
-                      }}
-                      className="w-full px-2 py-1 mb-1 border"
-                    />
-                    <textarea
-                      placeholder="Text"
-                      value={bodyItem.text || ""}
-                      onChange={(e) => {
-                        const updated = [...article.content];
-                        updated[index].contentbody![idx] = {
-                          ...updated[index].contentbody![idx],
-                          text: e.target.value,
-                        };
-                        handleChange(article._id!, "content", updated);
-                      }}
-                      className="w-full px-2 py-1 mb-1 border"
-                    />
+                      }} className="w-full px-2 py-1 mb-1 border" />
+                    <textarea placeholder="Text" value={bodyItem.text || ""} onChange={(e) => {
+                      const updated = [...article.content];
+                      updated[index].contentbody![idx] = {
+                        ...updated[index].contentbody![idx],
+                        text: e.target.value,
+                      };
+                      handleChange(article._id!, "content", updated);
+                    }} className="w-full px-2 py-1 mb-1 border" />
                   </div>
-                ))}
+                ))
+              }
 
               {contentItem.type === "main" && (
-                <button
-                  type="button"
-                  onClick={() => addContentBodyItem(article, index)}
-                  className="px-2 py-1 mt-1 border"
-                >
-                  Add Nested Content
+                <button type="button" onClick={() => addContentBodyItem(article, index)} className="px-2 py-1 mt-1 border" >
+                  Tilføj "main" nested paragraph + headline
                 </button>
               )}
             </div>
           ))}
-
+          {/* EDIT ADD CONTENT ITEM */}
           <button type="button" onClick={() => addContentItem(article)} className="px-3 py-2 mb-2 border">
             Add Content Item
           </button>
-
+          {/* EDIT TOGGLE LANDINGPAGE */}
           <label className="flex items-center gap-2 mb-2">
-            <input
-              type="checkbox"
-              checked={article.isLandingpage}
-              onChange={() => toggleLandingPage(article)}
-            />
+            <input type="checkbox" checked={article.isLandingpage} onChange={() => toggleLandingPage(article)} />
             Landing Page
           </label>
-
+          
+          {/* EDIT SAVE / DELETE ARTICLE */}
           <div className="flex gap-2">
-            <button
-              type="button"
-              onClick={(e) => {
-                e.preventDefault();
+            <button type="button" onClick={(e) => {
                 updateArticle(article);
-              }}
-              className="px-3 py-2 text-white bg-green-600"
-            >
+              }} className="px-3 py-2 text-white bg-green-600" >
               Save
             </button>
-            <button
-              type="button"
-              onClick={(e) => {
-                e.preventDefault();
-                deleteArticle(article);
-              }}
-              className="px-3 py-2 text-white bg-red-600"
-            >
+            <button type="button" onClick={(e) => {
+              deleteArticle(article);
+            }} className="px-3 py-2 text-white bg-red-600" >
               Delete
             </button>
           </div>
